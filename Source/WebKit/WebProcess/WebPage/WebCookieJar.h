d655adcde3856c24f1c2fb42c4eeef121fc13ffe
Diego Pino Garcia <dpino@igalia.com>
2025-01-15 09:35:49 +0100

    Resolve conflicts

--- Source/WebKit/WebProcess/WebPage/WebCookieJar.h
+++ Source/WebKit/WebProcess/WebPage/WebCookieJar.h
@@ -76,16 +76,12 @@ public:
 
     void clearCache() final;
 
-<<<<<<< HEAD
+    void setCookieFromResponse(WebCore::ResourceLoader&, const String& setCookieValue);
+
 #if HAVE(ALLOW_ONLY_PARTITIONED_COOKIES)
     void setOptInCookiePartitioningEnabled(bool);
 #endif
 
-||||||| parent of f74620cda50b (chore(webkit): bootstrap build #2124)
-=======
-    void setCookieFromResponse(WebCore::ResourceLoader&, const String& setCookieValue);
-
->>>>>>> f74620cda50b (chore(webkit): bootstrap build #2124)
 private:
     WebCookieJar();
 
